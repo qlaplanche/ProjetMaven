7eab54e631a6d871f25b8138ac0b3382a2119f83
Quentin Laplanche <quentin.laplanche@outlook.fr>
2017-04-11 22:45:44 +0200

    Ajout code + ajout tache build maven

--- ql/src/main/java/ql/App.java
+++ ql/src/main/java/ql/App.java
@@ -4,10 +4,13 @@ package ql;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Résultat : " + max(4, 5));
+    }
+
+    public static int max(int a, int b) {
+        return a > b ? a : b;
     }
 }
