31fba4e15b9eb4e11e1066227e54b315ba40d82b
Quentin Laplanche <quentin.laplanche@outlook.fr>
2017-04-12 11:14:14 +0200

    Fin OpenCSV Ajout deuxieme release

--- ql/README.md
+++ ql/README.md
@@ -10,3 +10,20 @@ Par Quentin Laplanche.
 
     - Comme le stagiaire a modifié le code. Celui-ci ne passe plus les tests unitaires que nous avons écrits.
 Maven nous affiche donc : `on project ql: There are test failures.` et refuse de faire une release.
+
+## Partie 2:
+
+- ### Exercice 23 
+```Java
+    java -jar target/marelease.jar
+Hello World!
+Résultat : 5
+Exception in thread "main" java.lang.NoClassDefFoundError: com/opencsv/CSVReader
+        at ql.App.main(App.java:19)
+Caused by: java.lang.ClassNotFoundException: com.opencsv.CSVReader
+        at java.net.URLClassLoader.findClass(URLClassLoader.java:381)
+        at java.lang.ClassLoader.loadClass(ClassLoader.java:424)
+        at sun.misc.Launcher$AppClassLoader.loadClass(Launcher.java:331)
+        at java.lang.ClassLoader.loadClass(ClassLoader.java:357)
+        ... 1 more
+    ```
--- ql/src/main/java/ql/App.java
+++ ql/src/main/java/ql/App.java
@@ -1,5 +1,11 @@
 package ql;
 
+import java.util.*;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.opencsv.*;
+
 /**
  * Hello world!
  *
@@ -8,10 +14,27 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Résultat : " + max(4, 5));
+
+        try {
+            CSVReader reader = new CSVReader(new FileReader("data.csv"));
+            int maximum = 0;
+            List<String[]> myEntries = reader.readAll();
+
+            for (String[] entry : myEntries) {
+                for (int i = 0; i < entry.length; i++) {
+                    int numData = Integer.parseInt(entry[i]);
+                    maximum = max(numData, maximum);
+                    System.out.println("Nombre lu : " + numData);
+                }
+            }
+            System.out.println("Max : " + maximum);
+        } catch (IOException exception) {
+            System.err.println("Error reading the csv" + exception);
+        }
     }
 
     public static int max(int a, int b) {
-        //return a > b ? a : b;
-        return a; // Encore un stagiaire ... 
+        return a > b ? a : b;
+        // return a; // Encore un stagiaire ... 
     }
 }
